333766bd80bc49574036f1ed6cb0bb00c3574c15
yo-yan <yooyan0512@gmail.com>
2021-03-22 23:03:19 +0900

    画面遷移、firebase接続

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { createData } from '../config/firebase'
 
 const Login = () => {
+
+    const handleClick = async () => {
+        await createData()
+        console.log('createUser')
+    }
+
     return (
         <div>
-
+            Login
+            <button onClick={handleClick}>CREATE USER</button>
         </div>
     )
 }
