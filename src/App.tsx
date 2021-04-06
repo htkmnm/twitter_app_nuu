441a8643f13f2c0889d29adb2f027fe751aa8b94
はつき みなみ <hatsukiminami@hatsuki-no-MacBook-Air.local>
2021-04-06 11:33:52 +0900

    topic-0004途中

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './components/Login'
-import Main from './components/Main'
-import SimpleModal from './components/SimpleModal'
-import Create from './components/Create'
-import ResetPassword from './components/ResetPassword'
+import Login from './components/Login';
+import Main from './components/Main';
+import SimpleModal from './components/SimpleModal';
+import Create from './components/Create';
+import ResetPassword from './components/ResetPassword';
 
 
 function App() {
