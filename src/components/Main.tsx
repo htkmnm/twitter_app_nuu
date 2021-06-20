89cbee13228c5af368c56c05d5e31b0cfe1736b9
はつき みなみ <hatsukiminami@hatsuki-no-MacBook-Air.local>
2021-06-20 13:21:14 +0900

    最新

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -12,8 +12,8 @@ import { Avatar } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import PhotoCamera from '@material-ui/icons/PhotoCamera';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Nuuheader from './Nuuheader'
-
+import Nuuheader from './Nuuheader';
+import Message from '../components/Message';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -27,8 +27,6 @@ const useStyles = makeStyles((theme: Theme) =>
         },
     }),
 );
-
-import Message from '../components/Message'
 
 const Main = ({ name }: any) => {
     const [string, setString] = useState<any>('');
@@ -61,11 +59,9 @@ const Main = ({ name }: any) => {
                 // No user is signed in.
             }
         });
-      
         //readData()
         inputEl.current.focus();
     }, []);
-    });
 
     const handleClick = async () => {
         sendMessage(username!, string!, avater!)
@@ -152,35 +148,36 @@ const Main = ({ name }: any) => {
             <div className='main'>
                 <div className='design'>
                     <Avatar alt="Remy Sharp" src={avater!} />{username}</div>
-            <header>
-                <TextField className='text' type='text' inputRef={inputEl} id="tweet" label="tweet" value={string} onChange={e => setString(e.target.value)} />
-                <Button variant="outlined" onClick={handleClick}>送信</Button>
-                <input type="file" onChange={(e) => inputFile(e.target.files)} />
-            </header>
-            <main>
-                {tweet && tweet.map((element: any, index: any) => {
-                    return (
-                        <ul key={index}>
-                            <Message
-                                username={element.name}
-                                avater={element.avater}
-                                string={element.string}
-                                tweet={element.message}
-                                createAt={element.createAt} />
-                        </ul>
-                    );
-                })}
-            </main>
-            <div className='header'>
-                <h1>Nuu.Main</h1>
+                <header>
+                    <TextField className='text' type='text' inputRef={inputEl} id="tweet" label="tweet" value={string} onChange={e => setString(e.target.value)} />
+                    <Button variant="outlined" onClick={handleClick}>送信</Button>
+                    <input type="file" onChange={(e) => inputFile(e.target.files)} />
+                </header>
+                <main>
+                    {tweet && tweet.map((element: any, index: any) => {
+                        return (
+                            <ul key={index}>
+                                <Message
+                                    username={element.name}
+                                    avater={element.avater}
+                                    string={element.string}
+                                    tweet={element.message}
+                                    createAt={element.createAt} />
+                            </ul>
+                        );
+                    })}
+                </main>
+                <div className='header'>
+                    <h1>Nuu.Main</h1>
 
 
-            </div>
-            <div className='main'>
-                <button onClick={logout}>Logout</button>
-            </div>
-            <div className='footer'>
+                </div>
+                <div className='main'>
+                    <button onClick={logout}>Logout</button>
+                </div>
+                <div className='footer'>
 
+                </div>
             </div>
         </div>
     )
